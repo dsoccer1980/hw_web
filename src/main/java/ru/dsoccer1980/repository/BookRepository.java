91721abe153c0f21cb73f957fce2f14cecb8bd86
dsoccer1980 <dsoccer1980@gmail.com>
2019-05-16 15:01:47 +0300

    Update Repositories tests

--- src/main/java/ru/dsoccer1980/repository/BookRepository.java
+++ src/main/java/ru/dsoccer1980/repository/BookRepository.java
@@ -1,14 +1,13 @@
 package ru.dsoccer1980.repository;
 
 import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
+import reactor.core.publisher.Flux;
 import ru.dsoccer1980.domain.Book;
-
-import java.util.List;
 
 
 public interface BookRepository extends ReactiveMongoRepository<Book, String>, BookRepositoryCustom {
 
-    List<Book> findByAuthorId(String id);
+    Flux<Book> findByAuthorId(String id);
 
-    List<Book> findByGenreId(String id);
+    Flux<Book> findByGenreId(String id);
 }
