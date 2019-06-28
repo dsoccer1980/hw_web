78154e7d7393ea3017f378fde64243603e93297c
dsoccer1980 <dsoccer1980@gmail.com>
2019-06-28 15:07:54 +0300

    Read banned words from file

--- src/main/java/ru/dsoccer1980/service/BookFilterService.java
+++ src/main/java/ru/dsoccer1980/service/BookFilterService.java
@@ -1,25 +1,59 @@
 package ru.dsoccer1980.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import ru.dsoccer1980.domain.Book;
+import ru.dsoccer1980.util.config.YamlProps;
 
-import java.util.List;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.URLDecoder;
+import java.nio.charset.StandardCharsets;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Set;
 
 @Service
 public class BookFilterService {
 
-    private final List<String> bannedWords = List.of(
-            "shit",
-            "nude",
-            "porno"
-    );
-    private final String REPLACING_STRING = "XXX";
+    private final YamlProps yamlProps;
+    private final String FILENAME;
+    private final String REPLACING_STRING;
+
+    @Autowired
+    public BookFilterService(YamlProps yamlProps) {
+        this.yamlProps = yamlProps;
+        FILENAME = URLDecoder.decode(yamlProps.getBannedWordsFilename(), StandardCharsets.UTF_8);
+        REPLACING_STRING = yamlProps.getReplacingString();
+    }
 
     public Book filter(Book book) {
+        Set<String> bannedWords = getBannedWordsList();
         String bookName = book.getName().toLowerCase();
         bannedWords.stream()
                 .filter(bookName::contains)
                 .forEach(word -> book.setName(bookName.replaceAll(word, REPLACING_STRING)));
         return book;
+    }
+
+    private Set<String> getBannedWordsList() {
+        try (InputStream inputStream = BookFilterService.class.getClassLoader().getResourceAsStream(FILENAME)) {
+            if (inputStream == null) {
+                return Collections.emptySet();
+            }
+            try (BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream, StandardCharsets.UTF_8))) {
+                Set<String> bannedWords = new HashSet<>();
+                String word;
+                while ((word = reader.readLine()) != null) {
+                    bannedWords.add(word);
+                }
+                return bannedWords;
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+            return Collections.emptySet();
+        }
     }
 }
